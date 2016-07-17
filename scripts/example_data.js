69a49e1331b3cfca1a222b4e843e991c79f197c2
Jakub Ciechowski <sianex@gmail.com>
2016-07-17 10:33:59 +0200

    obsolete script

--- scripts/example_data.js
+++
@@ -1,8 +0,0 @@
-db.players.insert({"_id":"1","Position":"G","Number":"0","Name":"Bradley, Avery","Height":"6 ft 2 in (1.88 m)","Weight":"180 lb (82 kg)","DOB":"1990–11–26","Team":"Boston Celtics"});
-db.players.insert({"_id":"2","Position":"F","Number":"99","Name":"Crowder, Jae Injured","Height":"6 ft 6 in (1.98 m)","Weight":"235 lb (107 kg)","DOB":"1990–07–06","Team":"Boston Celtics"});
-db.players.insert({"_id":"3","Position":"G","Number":"28","Name":"Hunter, R. J.","Height":"6 ft 5 in (1.96 m)","Weight":"185 lb (84 kg)","DOB":"1993–10–24","Team":"Boston Celtics"});
-db.players.insert({"_id":"4","Position":"F","Number":"8","Name":"Jerebko, Jonas","Height":"6 ft 10 in (2.08 m)","Weight":"231 lb (105 kg)","DOB":"1987–03–02","Team":"Boston Celtics"});
-db.players.insert({"_id":"5","Position":"F","Number":"90","Name":"Johnson, Amir","Height":"6 ft 9 in (2.06 m)","Weight":"240 lb (109 kg)","DOB":"1987–05–01","Team":"Boston Celtics"});
-db.players.insert({"_id":"6","Position":"F","Number":"55","Name":"Mickey, Jordan (DL)","Height":"6 ft 8 in (2.03 m)","Weight":"235 lb (107 kg)","DOB":"1994–07–09","Team":"Boston Celtics"});
-db.teams.insert({Name: "Pagórek Oliwa",GM: "sianeczkarz",Players: ["1", "2", "3"]});
-db.teams.insert({Name: "Bór Witomino",GM: "Kobas",Players: ["4", "5", "6"]});
